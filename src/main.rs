d9f702ca28b15cef5109c1603d87064419e9f49d
John Barker <dev@j16r.net>
2019-06-27 14:53:00 -0600

    Add initial example from coinbase_pro_rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate coinbase_pro_rs;
+
+use coinbase_pro_rs::{Public, Sync, SANDBOX_URL};
+
 fn main() {
-    println!("Hello, world!");
+    let client: Public<Sync> = Public::new(SANDBOX_URL);
+    let time = client.get_time().unwrap();
+    println!("Coinbase.time: {}", time.iso);
 }

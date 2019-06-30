b6536570fd45d33ab1c160e8f39af2f19fbb0b1c
John Barker <dev@j16r.net>
2019-06-29 20:39:59 -0600

    Add wallet which contains lots, and can handle selling and calculate cost

--- src/main.rs
+++ src/main.rs
@@ -170,8 +170,95 @@ fn load_config() -> io::Result<Config> {
     Ok(config)
 }
 
+struct Wallet {
+    lots: Vec<Lot>,
+}
+
+impl Wallet {
+    fn new() -> Wallet {
+        Wallet { lots: Vec::new() }
+    }
+
+    fn add_lot(&mut self, amount: f64, unit_cost: f64, date: chrono::NaiveDate) {
+        self.lots.push(Lot {
+            amount: amount,
+            unit_cost: unit_cost,
+            date_of_purchase: date,
+        })
+    }
+
+    fn total_value(&self) -> f64 {
+        self.lots.iter().map(|lot| lot.amount * lot.unit_cost).sum()
+    }
+
+    fn sell(&mut self, amount: f64) -> Sale {
+        let mut date_of_purchase: Option<chrono::NaiveDate> = None;
+
+        let mut total_cost = 0.0;
+        let mut lots_consumed = 0;
+        let mut amount_to_consume = amount;
+        for mut lot in self.lots.iter_mut() {
+            if date_of_purchase.is_none() {
+                date_of_purchase = Some(lot.date_of_purchase);
+            }
+
+            if amount_to_consume < lot.amount {
+                lot.amount -= amount_to_consume;
+                total_cost += amount_to_consume * lot.unit_cost;
+                break;
+            }
+
+            total_cost += lot.amount * lot.unit_cost;
+            amount_to_consume -= lot.amount;
+            lots_consumed += 1;
+        }
+
+        // Remove all consumed lots
+        self.lots.drain(..lots_consumed);
+
+        Sale {
+            cost_basis: total_cost,
+            date_of_purchase: date_of_purchase.unwrap(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Lot {
+    amount: f64,
+    unit_cost: f64,
+    date_of_purchase: chrono::NaiveDate,
+}
+
+#[derive(Debug)]
+struct Sale {
+    cost_basis: f64,
+    date_of_purchase: chrono::NaiveDate,
+}
+
+#[test]
+fn test_wallet_sell() {
+    let mut wallet = Wallet::new();
+
+    wallet.add_lot(10.0, 1.0, chrono::NaiveDate::from_yo(2018, 1));
+    wallet.add_lot(10.0, 2.0, chrono::NaiveDate::from_yo(2018, 2));
+    wallet.add_lot(10.0, 3.0, chrono::NaiveDate::from_yo(2018, 3));
+
+    let sale1 = wallet.sell(5.0);
+    assert_eq!(sale1.cost_basis, 5.0);
+    assert_eq!(sale1.date_of_purchase, chrono::NaiveDate::from_yo(2018, 1));
+
+    let sale2 = wallet.sell(10.0);
+    assert_eq!(sale2.cost_basis, 15.0);
+    assert_eq!(sale2.date_of_purchase, chrono::NaiveDate::from_yo(2018, 1));
+
+    let sale3 = wallet.sell(10.0);
+    assert_eq!(sale3.cost_basis, 25.0);
+    assert_eq!(sale3.date_of_purchase, chrono::NaiveDate::from_yo(2018, 2));
+}
+
 fn report(year: u16) -> Result<(), Box<Error>> {
-    let mut accounts: HashMap<String, f64> = HashMap::new();
+    let mut wallets: HashMap<String, Wallet> = HashMap::new();
 
     let mut rdr = csv::Reader::from_reader(io::stdin());
 
@@ -200,18 +287,26 @@ fn report(year: u16) -> Result<(), Box<Error>> {
             continue;
         }
 
-        let balance = accounts
+        let wallet = wallets
             .entry(line_item.get(2).unwrap().into())
-            .or_insert_with(|| 0.0);
+            .or_insert_with(|| Wallet::new());
 
-        *balance += line_item.get(7).unwrap().parse::<f64>().unwrap();
+        let amount = line_item.get(3).unwrap().parse::<f64>().unwrap();
+        let proceeds = line_item.get(7).unwrap().parse::<f64>().unwrap();
+        let date_of_sale_tz =
+            chrono::DateTime::parse_from_rfc3339(line_item.get(8).unwrap()).unwrap();
+        let date_of_sale = date_of_sale_tz.naive_utc().date();
 
-        let date_of_sale = chrono::DateTime::parse_from_rfc3339(line_item.get(8).unwrap()).unwrap();
+        let rate = line_item.get(5).unwrap().parse::<f64>().unwrap();
+        wallet.add_lot(amount, rate, date_of_sale);
+
         let year_of_sale = date_of_sale.year();
-
         if year_of_sale == year as i32 {
-            let proceeds = line_item.get(7).unwrap().parse::<f64>().unwrap();
-            let cost_basis = 0.0;
+            let Sale {
+                cost_basis,
+                date_of_purchase,
+            } = wallet.sell(amount);
+
             let gain = proceeds - cost_basis;
 
             total_proceeds += proceeds;
@@ -224,7 +319,7 @@ fn report(year: u16) -> Result<(), Box<Error>> {
                     line_item.get(2).unwrap(),
                     line_item.get(1).unwrap()
                 ),
-                "",
+                &date_of_purchase.format("%D").to_string(),
                 &date_of_sale.format("%D").to_string(),
                 &format_amount(proceeds),
                 &format_amount(cost_basis),
@@ -244,8 +339,8 @@ fn report(year: u16) -> Result<(), Box<Error>> {
         &format_amount(total_gain),
     ])?;
 
-    for (currency, balance) in accounts {
-        eprintln!("Currency {:?} balance = {:?}", currency, balance);
+    for (currency, wallet) in wallets {
+        eprintln!("Wallet {:?} balance = {:?}", currency, wallet.total_value());
     }
 
     writer.flush()?;
